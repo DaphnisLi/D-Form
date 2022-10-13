8fb6b6ce94befc287a1d1b911c1ec74961b64427
DaphnisLi <1872758972@qq.com>
2022-10-13 10:24:28 +0800

    feat: 项目初始化

---
+++ .umirc.ts
@@ -0,0 +1,17 @@
+// config: https://d.umijs.org/config
+
+import { defineConfig } from 'dumi'
+
+export default defineConfig({
+  title: 'D-Form',
+  favicon:
+    'https://s2.loli.net/2022/08/14/51A6SiswhVeGnRL.png',
+  logo: 'https://s2.loli.net/2022/08/14/51A6SiswhVeGnRL.png',
+  outputPath: 'docs',
+  mode: 'site',
+  navs: [
+    null,
+    { title: '源码', path: 'https://github.com/DaphnisLi/D-Form.git' }
+  ],
+  mfsu: {},
+})
